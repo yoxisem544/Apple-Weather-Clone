36dffd0aead1ee53db266e652985b5d58f481a30
David Lin <yoxisem544@gmail.com>
2023-04-19 19:59:27 +0800

    Initial Commit

---
+++ WeatherClone/WeatherCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCloneApp.swift
+//  WeatherClone
+//
+//  Created by David on 2023/4/19.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
